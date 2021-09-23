3dd580786a650b2a1ed2d3790fd3e1865b9cc666
svinothini24 <vinothiniselvam24@gmail.com>
2021-09-23 12:38:19 +0530

    fix(zabbix): rebase and fix conflicts

--- zabbixDBAdapter.py
+++ zabbixDBAdapter.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python
+"""Script to poll metrics from MySQL Database and push them to Wavefront."""
 
 from __future__ import print_function
-from wavefront_sdk.client_factory import WavefrontClientFactory
 
 import os
 import sys
-import mysql.connector
 import re
 import time
 import datetime
 import socket
 import signal
 import itertools
+import mysql.connector
 
+from wavefront_sdk.client_factory import WavefrontClientFactory
 
 # USER CONFIGURATION ###
 
-ZABBIX_PREFIX = "zabbix."    # All metrics will be prefixed with this text. Please ensure it has a trailing .
+# All metrics will be prefixed with this text. Please ensure it has a trailing
+ZABBIX_PREFIX = "zabbix."
 
-POLL_INTERVAL = 60           # Frequency at which records will be retrieved from the DB in seconds
-LIMIT = 10000                # Limit the amount of records that will be pulled from the DB on each POLL_INTERVAL
+# Frequency at which records will be retrieved from the DB in seconds
+POLL_INTERVAL = 60
 
-SEND_TO_WF = False            # Set to False to print values rather than sending to Wavefront
+# Limit the amount of records that will be pulled from the DB on each
+# POLL_INTERVAL
+LIMIT = 10000
+
+# Set to False to print values rather than sending to Wavefront
+SEND_TO_WF = False
+
 WAVEFRONT_PROXY_HOST = "localhost"
 WAVEFRONT_PROXY_PORT = 2878
 
@@ -30,15 +38,18 @@ DB_DATABASE = "zabbix"
 DB_UNAME = "root"
 DB_PW = "password"
 
-# Store the latest Epoch time that has already been sent to Wavefront in these files. There is a separate file for each
-# Zabbix telemetry table - history and history_uint The "history" table stores metrics with float/double values and the
-# "history_uint" table stores metrics with Integer values.
+# Store the latest Epoch time that has already been sent to Wavefront in
+# these files. There is a separate file for each Zabbix telemetry table -
+# history and history_uint The "history" table stores metrics with
+# float/double values and the "history_uint" table stores metrics
+# with Integer values.
 HISTORY_CLOCK_FILEPATH = "last_history_clock.hist"
 HISTORYUINT_CLOCK_FILEPATH = "last_historyuint_clock.hist"
 
 # END USER CONFIGURATION ###
 
-# SQLs. The results of SQL_QUERY_ITEMIDS are used within SQL_QUERY_HISTORY to force MySQL to use the history_1 index
+# SQLs. The results of SQL_QUERY_ITEMIDS are used within SQL_QUERY_HISTORY
+# to force MySQL to use the history_1 index
 # which is created by default by the Zabbix install.
 SQL_QUERY_ITEMIDS = "select distinct itemid from items"
 
@@ -53,7 +64,8 @@ SQL_QUERY_HISTORY = """SELECT hi.clock, hi.value, h.host, i.key_ FROM
 
 
 def read_last_clock_file(file):
-    """Return the clock (Unixtime) from the specified file or current time if the file doesn't exist"""
+    """Return the clock (Unixtime) from the specified file or current time
+if the file doesn't exist"""
     last_clock = int(time.time())
     if os.path.isfile(file):
         try:
@@ -83,8 +95,8 @@ def write_last_clock_file(file, clock):
 
 
 def get_db_connection():
-    """Get a connection to the DB. We do this per query to ensure we always have a live
-connection."""
+    """Get a connection to the DB. We do this per query to ensure we always
+have a live connection."""
     conn = mysql.connector.connect(user=DB_UNAME, password=DB_PW,
                                    host=DB_SERVER, database=DB_DATABASE,
                                    autocommit=True)
@@ -92,11 +104,11 @@ connection."""
 
 
 def get_wavefront_client():
-    """Connect to the Wavefront Proxy. We do this per connection to ensure we always have a
-live connection."""
+    """Connect to the Wavefront Proxy. We do this per connection to ensure we
+always have a live connection."""
     client_factory = WavefrontClientFactory()
     client_factory.add_client(
-    url="proxy://{}:{}".format(WAVEFRONT_PROXY_HOST, WAVEFRONT_PROXY_PORT),
+        url="proxy://{}:{}".format(WAVEFRONT_PROXY_HOST, WAVEFRONT_PROXY_PORT),
         max_queue_size=50000,
         batch_size=10000,
         flush_interval_seconds=5)
@@ -115,12 +127,16 @@ Query both the history and history_uint tables."""
     # Process the history table which contains floating point metrics
     float_rows = query_db(HISTORY_TABLE, history_clock, cursor)
     float_points_count = len(float_rows)
-    history_clock = process_and_send_metrics(float_rows, history_clock, wavefront_sender)
+    history_clock = process_and_send_metrics(
+        float_rows, history_clock, wavefront_sender)
 
     # Process the history_uint table which contains integer metrics
     int_rows = query_db(HISTORY_TABLE_UINT, historyuint_clock, cursor)
     int_points_count = len(int_rows)
-    historyuint_clock = process_and_send_metrics(int_rows, historyuint_clock, wavefront_sender)
+    historyuint_clock = process_and_send_metrics(
+        int_rows,
+        historyuint_clock,
+        wavefront_sender)
 
     cursor.close()
     conn.close()
@@ -134,8 +150,9 @@ def query_db(history_table_name, clock, cursor):
     """Query the DB for the given table and clock time and return any rows"""
     cursor.execute(SQL_QUERY_ITEMIDS)
 
-    # The Python MySQL connector doesn't natively handle binding a list to a SQL IN clause
-    # so that is handled here add the correct number of %s symbols to the SQL string
+    # The Python MySQL connector doesn't natively handle binding a list
+    # to a SQL IN clause so that is handled here add the correct number of %s
+    # symbols to the SQL string
     itemids = [x[0] for x in cursor.fetchall()]
     in_p = ','.join(itertools.repeat('%s', len(itemids)))
     sql = SQL_QUERY_HISTORY % (history_table_name, in_p, clock, LIMIT)
@@ -145,11 +162,12 @@ def query_db(history_table_name, clock, cursor):
 
 
 def process_and_send_metrics(rows, latest_clock, wavefront_sender, tags):
-    """Convert each row in rows into the Wavefront format and send to the Wavefront
-proxy. Return the latest clock value found (which will be unchanged if rows was empty)"""
+    """Convert each row in rows into the Wavefront format and send to the
+Wavefront proxy. Return the latest clock value found (which will be unchanged
+if rows was empty)"""
     for (clock, value, host, itemkey) in rows:
-        # These isinstance checks will only return true with Python3. See this issue:
-        # http://sourceforge.net/p/mysql-python/bugs/289/
+        # These isinstance checks will only return true with Python3.
+        # See this issue: http://sourceforge.net/p/mysql-python/bugs/289/
         if isinstance(host, (bytes, bytearray)):
             host = host.decode()
         if isinstance(itemkey, (bytes, bytearray)):
@@ -163,10 +181,10 @@ proxy. Return the latest clock value found (which will be unchanged if rows was 
             latest_clock = clock
         # Wavefront metric names must include at least one .
         if "." not in metric:
-            warning("Cannot process Zabbix item with key_: {} as it contains no . character".format(itemkey))
+            warning("Cannot process Zabbix item with key_: {} as it contains no . character:".format(itemkey))
             continue
 
-        # wavefront_sender will be None if configuration option SEND_TO_WF is False
+        # wavefront_sender will be None if SEND_TO_WF is False
         if wavefront_sender:
             wavefront_sender.send_metric(metric, value, clock, host, tags)
         else:
@@ -178,12 +196,12 @@ proxy. Return the latest clock value found (which will be unchanged if rows was 
 
 
 def convert_key_to_wf_metric(key):
-    """A Wavefront metric name can be: 'Any lowercase, alphanumeric, dot-separated value. May also
-include dashes (-) or underscores (_)'
+    """A Wavefront metric name can be: 'Any lowercase, alphanumeric,
+dot-separated value. May also include dashes (-) or underscores (_)'
 
-For the purposes of sending data from Zabbix to Wavefront we replace any whitespace or punctuation
-other than dash and underscore from the Zabbix key_ with a single . and convert the whole string to
-lower case. """
+For the purposes of sending data from Zabbix to Wavefront we replace any
+whitespace or punctuation other than dash and underscore from the Zabbix key_
+with a single . and convert the whole string to lower case. """
     metric = replace_punctuation_and_whitespace(key)
     metric = remove_trailing_dots(metric)
     metric = just_one_dot(metric)
@@ -193,33 +211,38 @@ lower case. """
 
 
 def replace_punctuation_and_whitespace(text):
-    """Replace occurrences of punctuation (other than . - _) and any consecutive white space with ."""
-    rx = re.compile(r"[^\w\s\-.]|\s+")
-    return rx.sub(".", text)
+    """Replace occurrences of punctuation (other than . - _) and
+any consecutive white space with ."""
+    regex = re.compile(r"[^\w\s\-.]|\s+")
+    return regex.sub(".", text)
 
 
 def just_one_dot(text):
-    """Some Zabbix metrics can end up with multiple . characters. Replace with a single one"""
-    rx = re.compile(r"\.+")
-    return rx.sub(".", text)
+    """Some Zabbix metrics can end up with multiple . characters.
+Replace with a single one"""
+    regex = re.compile(r"\.+")
+    return regex.sub(".", text)
+
 
 def prefix_metric(metric):
-    """Apply metric prefix if the collected metrics does not start with 'zabbix.'"""
+    """Apply metric prefix if the collected metrics does
+not start with 'zabbix.'"""
     if metric.startswith(ZABBIX_PREFIX):
-        return (metric)
+        return metric
     else:
-        return (ZABBIX_PREFIX + metric)
+        return ZABBIX_PREFIX + metric
 
 
 def remove_trailing_dots(text):
     """Remove any trailing . characters from a metric name"""
-    rx = re.compile(r"\.+$")
-    return rx.sub("", text)
+    regex = re.compile(r"\.+$")
+    return regex.sub("", text)
 
 
 def format_clock(clock):
     """Return the clock in a human readable format"""
-    return datetime.datetime.fromtimestamp(int(clock)).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.datetime.fromtimestamp(int(clock)).strftime(
+        '%Y-%m-%d %H:%M:%S')
 
 
 def warning(*msgs):
@@ -233,6 +256,7 @@ def error(*msgs):
 
 
 def signal_handler(signal, frame):
+    """Print the final Float Clock Time"""
     print("Wrapping up. Final Float Clock Time: {}. Final Int Clock Time: {}.".format(
         history_clock, historyuint_clock))
 
@@ -242,8 +266,8 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 if __name__ == "__main__":
-    # The clocks store the epoch time that has already been sent to Wavefront for each Zabbix
-    # telemetry table (history and history_uint)
+    # The clocks store the epoch time that has already been sent
+    # to Wavefront for each Zabbix telemetry table (history and history_uint)
     history_clock = read_last_clock_file(HISTORY_CLOCK_FILEPATH)
     historyuint_clock = read_last_clock_file(HISTORYUINT_CLOCK_FILEPATH)
 
@@ -255,15 +279,19 @@ if __name__ == "__main__":
         wavefront_sender = get_wavefront_client()
 
     # Loop forever (unless killed by SIGINT) or exception caught
-    while(True):
+    while True:
         try:
-            history_clock, historyuint_clock = fetch_next_metrics(history_clock, historyuint_clock, wavefront_sender)
+            history_clock, historyuint_clock = fetch_next_metrics(
+                history_clock, historyuint_clock, wavefront_sender)
 
             msg = "Latest Float Point: {}. Latest Integer Point: {}."
-            print(msg.format(format_clock(history_clock), format_clock(historyuint_clock)))
+            print(msg.format(
+                format_clock(history_clock),
+                format_clock(historyuint_clock)))
 
             write_last_clock_file(HISTORY_CLOCK_FILEPATH, history_clock)
-            write_last_clock_file(HISTORYUINT_CLOCK_FILEPATH, historyuint_clock)
+            write_last_clock_file(
+                HISTORYUINT_CLOCK_FILEPATH, historyuint_clock)
             time.sleep(POLL_INTERVAL)
         except mysql.connector.Error as e:
             error("Please check your database configuration: {}".format(e))
